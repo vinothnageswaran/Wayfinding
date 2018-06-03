48f5805bffd0de0865dfa9d76508fdc0c89e2085
Vinoth <Vinoth@192.168.1.8>
2018-06-03 18:40:34 +0800

    First push

---
+++ src/Driver.java
@@ -0,0 +1,165 @@
+
+//import org.apache.commons.lang.StringEscapeUtils;
+
+
+import java.awt.HeadlessException;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+
+import org.sikuli.script.App;
+import org.sikuli.script.Button;
+import org.sikuli.script.Env;
+import org.sikuli.script.FindFailed;
+import org.sikuli.script.Key;
+import org.sikuli.script.KeyModifier;
+import org.sikuli.script.Location;
+import org.sikuli.script.Match;
+import org.sikuli.script.Pattern;
+import org.sikuli.script.Region;
+import org.sikuli.script.Screen;
+import org.sikuli.script.ScreenImage;
+import org.sikuli.basics.ImageLocator;
+import org.sikuli.basics.Settings;
+//import org.testng.annotations.Test;
+//import org.sikuli.basics.Settings;
+
+public class Driver{
+
+	
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) throws FindFailed, InterruptedException, UnsupportedFlavorException,IOException {
+
+
+	ImageLocator.setBundlePath("C:/temp/Images");
+	
+	String InputFile ="W://HIN//D&D//Projects//Release//J- Testing//11. PCH BOH Agility Test Planning//Integration//01 Interface Docs//03 RTLS//RFID//Source.xls";
+
+				
+	Screen s=new Screen();
+	
+
+	  Settings.OcrTextSearch=true;
+      Settings.OcrTextRead=true;
+	
+	
+
+	s.find("CORE.png"); //identify pause button
+
+	s.doubleClick("CORE.png");
+	
+	s.wait((double) 3.0);
+	
+System.out.println("CORE button clicked");
+
+ s.type("Uname.png" , "windowsadmin3");
+ s.type("Pword.png","Work1t");
+ 
+ s.click("Login.png");
+ 
+ //s.click("Yes.png");
+ 
+ //s.waitAll();
+ 
+ s.wait((double) 3.0);
+ 
+ s.click("Configuration.png");
+ 
+ s.click("Organisation123.png");
+ 
+ s.click("Wayfinding.png");
+ 
+ s.type("Location.png" , "L9");
+ 
+ s.click("GO.png");
+ 
+
+s.type( Key.TAB ); 
+ s.type( Key.TAB ); 
+ s.type( Key.TAB ); 
+ s.type( Key.TAB ); 
+ s.type( Key.TAB ); 
+ s.type( Key.TAB ); 
+ //s.type( Key.TAB ); 
+//s.type( Key.TAB ); 
+
+Table tes = new Table();
+
+/*
+for (int j=2;  j<716; j++)
+	
+	{
+	
+	*/
+
+for (int i =0; i<14 ; i++)
+	
+		{
+	s.type("a", KeyModifier.CTRL);
+
+	s.type("c", KeyModifier.CTRL);
+
+	String Keey= App.getClipboard();
+	
+	System.out.println("Key " + Keey);
+			
+	
+	
+	String Value = tes.getValue1(Keey, InputFile);
+	
+	System.out.println("Value" + Value);
+	s.type( Key.ENTER );
+	s.type( Key.TAB );
+	
+	s.type("a", KeyModifier.CTRL);
+	
+	s.type(Value);
+	s.type( Key.ENTER );
+	s.type( Key.TAB );
+	
+	
+	
+
+				}
+
+s.click("Save.png");
+
+	}
+	
+}
+	
+	
+
+/*
+
+s.type(Integer.toString(j));
+
+System.out.println("j" + j);
+
+
+s.type( Key.ENTER );
+s.type( Key.TAB );
+
+
+	
+		for (int k=0; k<69; k++)
+		
+		{
+s.type(Key.TAB, KeyModifier.SHIFT);
+s.wait((double) 1.0);
+		
+		
+		}
+	
+
+	
+	}
+
+ 
+	}	
+
+}
+	
+*/
---
+++ src/Table.java
@@ -0,0 +1,101 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+
+import jxl.Cell;
+import jxl.CellType;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+public class Table {
+
+        private String inputFile;
+
+        public void setInputFile(String inputFile) {
+                this.inputFile = inputFile;
+        }
+
+     
+        /*public HashMap read(HashMap values) throws IOException  {
+                File inputWorkbook = new File(inputFile);
+                Workbook w;
+                try {
+                        w = Workbook.getWorkbook(inputWorkbook);
+                        // Get the first sheet
+                        Sheet sheet = w.getSheet(0);
+                        // Loop over first 10 column and lines
+                        
+                        
+                                for (int i = 0; i < sheet.getRows(); i++) {
+                                        Cell cell = sheet.getCell(0, i);
+                                        Cell cell1 = sheet.getCell(1,i);
+                                        values.put(cell.getContents(),cell1.getContents());
+                                       
+                                 }
+
+                                
+                }
+                catch (Exception e)
+                {
+                	System.out.println(e);
+                }
+                      
+                return values;
+                
+        }
+        */
+        String getValue1(String Key, String InputFile)throws IOException  {
+        	
+        	//CORE test = new CORE();
+        	String Value ="";
+            HashMap<String, String> values =  new HashMap<>();
+            //test.setInputFile("c:/temp/COREtest1.xls");
+            //File inputWorkbook = new File("c:/temp/COREtest1.xls");
+            
+            File inputWorkbook = new File(InputFile);
+            Workbook w;
+            try {
+                    w = Workbook.getWorkbook(inputWorkbook);
+                    // Get the first sheet
+                    Sheet sheet = w.getSheet(0);
+                    // Loop over first 10 column and lines
+                    
+                    
+                            for (int i = 0; i < sheet.getRows(); i++) {
+                                    Cell cell = sheet.getCell(0, i);
+                                    Cell cell1 = sheet.getCell(1,i);
+                                    
+                                    
+                                    values.put(cell.getContents(),cell1.getContents());
+                                   
+                             }
+
+                            
+            }
+            catch (Exception e)
+            {
+            	System.out.println(e);
+            	
+            
+            }
+         Value = values.get(Key);
+           
+           //Value= values.containsKey(key);
+            
+           // Value= values.values();
+            return Value;
+            
+    }
+    
+      /*
+        public static void main(String[] args) throws IOException {
+                CORE test = new CORE();
+                HashMap<String, String> values =  new HashMap<>();
+                test.setInputFile("c:/temp/COREtest1.xls");
+                HashMap<String, String> value = test.read(values);
+                System.out.println(value.get("6"));
+        }
+*/
+}
